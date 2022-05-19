88b66c609979e3e45b223de0d7760e38e45008c0
kinza <bsef19a515@pucit.edu.pk>
2022-05-19 12:17:26 +0500

    2nd commit with design

--- app/src/main/java/com/example/myapplication7/MainActivity.java
+++ app/src/main/java/com/example/myapplication7/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.myapplication7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn= findViewById(R.id.button);
+
     }
 }
